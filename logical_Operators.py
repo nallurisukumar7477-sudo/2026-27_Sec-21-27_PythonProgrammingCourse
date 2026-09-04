d9cda04698d7757836b0d8142499e618be46f771
nallurisukumar7477-sudo <nallurisukumar7477@gmail.com>
2026-09-04 13:39:27 +0530

    Logical Operators Done

---
+++ logical_Operators.py
@@ -0,0 +1,27 @@
+a=1
+result=a>2 and a<5
+print("result for AND operators:",result)
+
+
+a=3
+result=a>2 and a<5
+print("result for AND operator:",result)
+
+
+a=6
+result=a>5 or a<0
+print("result for OR operator :",result)
+
+
+a=2
+result=a>5 or a<0
+print("result for OR operator:",result)
+
+
+a=43
+result=not(a>2 and a<10)
+print("result for NOT operator:",result)
+
+a=7
+result=not(a>2 and a<10)
+print("result for NOT operator:",result)
