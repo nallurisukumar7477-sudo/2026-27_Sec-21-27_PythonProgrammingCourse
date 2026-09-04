daeb1951db3c3811b950512c2f4f0a063d5b6521
nallurisukumar7477-sudo <nallurisukumar7477@gmail.com>
2026-09-04 13:51:39 +0530

    identity operators done

---
+++ identity_Operators.py
@@ -0,0 +1,10 @@
+#identity operators
+a=[66,77,88]
+b=a
+result= b is a #is identity operator
+print("result of",b,"is",a,"is",result)
+
+c=[11,22,33]
+d=[11,22,33]
+result= c is d #is identity operator
+print("result of",c,"is",d,"is",result)
