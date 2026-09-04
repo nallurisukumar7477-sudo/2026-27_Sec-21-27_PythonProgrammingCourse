38635184a15849c4d1d6f3555731d2d3463a2080
nallurisukumar7477-sudo <nallurisukumar7477@gmail.com>
2026-09-04 14:21:24 +0530

    Membership Operators Done

---
+++ membership_operators.py
@@ -0,0 +1,13 @@
+#membership operators
+a=[66,77,88]
+result= 66 in a #in membership operator
+print("result of 66 in",a,"is",result)
+
+result= 99 in a #in membership operator
+print("result of 99 in",a,"is",result)
+
+result= 66 not in a #not in membership operator
+print("result of 66 not in",a,"is",result)
+
+result= 99 not in a #not in membership operator
+print("result of 99 not in",a,"is",result)
