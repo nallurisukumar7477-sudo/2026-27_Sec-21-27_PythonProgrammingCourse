d521df0df7abf75b84dc0cb0a94938cd4a7ce09d
nallurisukumar7477-sudo <nallurisukumar7477@gmail.com>
2026-09-18 13:46:00 +0530

    This task done....

---
+++ Sum_of_two_numbers.py
@@ -0,0 +1,19 @@
+num1=int(input("Enter num1: "))
+num2=int(input("Enter num2: "))
+sum=num1+num2
+print("The sum is:", sum)
+
+num3=int(input("Enter num3: "))
+num4=int(input("Enter num4: "))
+sum2=num3+num4
+print("The sum is:", sum2)
+
+num5=int(input("Enter num5: "))
+num6=int(input("Enter num6: "))     
+sum3=num5+num6
+print("The sum is:", sum3)  
+
+num7=int(input("Enter num7: "))
+num8=int(input("Enter num8: ")) 
+sum4=num7+num8
+print("The sum is:", sum4)
