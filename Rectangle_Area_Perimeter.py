847c08401b62bcc63e67d8090cff1761f459e446
nallurisukumar7477-sudo <nallurisukumar7477@gmail.com>
2026-09-18 13:50:08 +0530

    This task Done....

---
+++ Rectangle_Area_Perimeter.py
@@ -0,0 +1,6 @@
+num1=int(input("Enter the num1:"))
+num2=int(input("Enter te num2:"))
+area=num1*num2
+perimeter=2*(num1+num2)
+print("Area of rectangle",area)
+print("perimeter of rectangle",perimeter)
