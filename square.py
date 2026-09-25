1c2e0ee1dd1b472282e0d8fb4087d5693039a815
nallurisukumar7477-sudo <nallurisukumar7477@gmail.com>
2026-09-25 14:55:18 +0530

    this taskn done

---
+++ square.py
@@ -0,0 +1,14 @@
+num1=int(input("Enter number:"))
+print("Square:",num1*num1)
+
+num2=int(input("Enter number:"))
+print("Square:",num2*num2)
+
+num3=int(input("Enter number:"))
+print("Square:",num3*num3)
+
+num4= int(input("Enter number:"))
+print("Square:",num4*num4)
+
+num5=int(input("Enter number:"))
+print("Square:",num5*num5)
